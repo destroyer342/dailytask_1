048125e461218fb2e6626ab6465c6668b084c04d
Rance Cañete <rancedestroyer2@gmail.com>
2022-11-01 09:55:32 +0800

    done task

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 //const userNames = ['Jesse', 'Tom', 'Anna']
-export default class App extends Component{
-  state= {
-    name:'Rance',
-    age:25,
-    favoriteColors:['Blue', 'Red', 'Yellow']
-  }
+export default class App extends Component {
+  state = {
+    name: "Rance",
+    age: 25,
+    favoriteColors: ["Blue", "Red", "Yellow"],
+  };
 
- renderListOfColors = (colors) => {
-    return colors.map(color => <li>{color}</li>)
-  }
+  renderListOfColors = (colors) => {
+    return colors.map((color) => <li>{color}</li>);
+  };
   handleClick = () => {
     this.setState({
-      name:'Aubry'
-    })
-  }
-  render(){
-    return(
-    <div>
-      <h2>My Favorite Colors:</h2>
-      <ul>
-        {this.renderListOfColors(this.state.favoriteColors)}
-      </ul>
-      <h2>Hello {this.state.name}</h2>
-      <button onClick={this.handleClick}>CLICK ME TO CHANGE NAME</button>
-      
-    </div>
-    )
+      name: "Aubry",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h2>My Favorite Colors:</h2>
+        <ul>{this.renderListOfColors(this.state.favoriteColors)}</ul>
+        <h2>Hello {this.state.name}</h2>
+        <button onClick={this.handleClick}>CLICK ME TO CHANGE NAME</button>
+      </div>
+    );
   }
 }
